ba087615cd42f0017081b59d3cb5d872e1dd9e3c
David Duron <86013234+emt2dev@users.noreply.github.com>
2023-05-18 17:41:13 -0400

    add comments

--- Documentation/drafts/vanillaConceptTesting/unitTesting/unitTests.js
+++ Documentation/drafts/vanillaConceptTesting/unitTesting/unitTests.js
@@ -1,5 +1,5 @@
 // On document load, load these name spaces.
 
 window.addEventListener('load', UnitTestGenerator.init) // starts the unit testing process in the console.
-window.addEventListener('load', FEN_Display.init) // 
-// window.addEventListener('load', FEN_Pieces.init) // 
+window.addEventListener('load', FEN_Display.init) // changes the FEN displayed on screen
+// window.addEventListener('load', FEN_Pieces.init) // populates array based on FEN
