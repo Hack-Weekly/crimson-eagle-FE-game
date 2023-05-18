bddc4e0885b192b6e5103fab8cfdf0bc5f85198b
David Duron <86013234+emt2dev@users.noreply.github.com>
2023-05-18 13:13:12 -0400

    began testing logic for moveValidation

---
+++ Documentation/drafts/vanillaConceptTesting/moveValidationVisualTest.js
@@ -0,0 +1,6 @@
+const fen = 'qrknbbnr/pppppppp/8/8/8/8/PPPPPPPP/QRKNBBNR w - - 0 1';
+
+const fenDisplay = document.querySelector("#fenDisplay");
+
+fenDisplay.dataset.fen = fen;
+fenDisplay.textContent = fenDisplay.dataset.fen;
