4f62bb838f078373f6042d4572437b4d1c580a7c
David Duron <86013234+emt2dev@users.noreply.github.com>
2023-05-19 10:04:24 -0400

    began draftComponents to refractor to react

---
+++ chess-app/src/app/draftComponents/draft_Footer.tsx
@@ -0,0 +1,9 @@
+
+export default function draft_Footer() {
+        return (
+            <footer className="pt-5 my-5 text-body-secondary border-top">
+            Created by the Bootstrap team · © 2023
+        </footer>
+    </body>
+        )
+}
