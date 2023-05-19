842cf3a9d3da72726c60cf9506061c2b07e76aed
Pfreym <spcompsys@gmail.com>
2023-05-19 17:13:06 +0800

    Added Clerk Auth and some folder structure for routing in nextjs. Empty files for now

--- chess-app/src/app/page.tsx
+++ chess-app/src/app/page.tsx
@@ -1,10 +1,18 @@
-
+import { SignUp, UserButton } from "@clerk/nextjs";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
-      <h1>Chess App</h1>
-
-      </div>
+    <div>
+      <UserButton afterSignOutUrl="/"/>
+    </div>
+    <div>
+      <Link href="/sign-in">Sign In</Link>
+    </div>
+    <div>
+      <Link href="/sign-up">Sign Up</Link>
+    </div>
+    </div>
   )
 }
