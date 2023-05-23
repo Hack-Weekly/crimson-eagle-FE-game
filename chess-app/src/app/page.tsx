f29695e7a7acfdfa04a4860be4fff0b7a1ee15c9
Pfreym <spcompsys@gmail.com>
2023-05-23 10:32:44 +0800

    daisyUI styling2

--- chess-app/src/app/page.tsx
+++ chess-app/src/app/page.tsx
@@ -5,8 +5,8 @@ export default function Home() {
   <div className="hero-content flex-col lg:flex-row">
     <img src="https://images.unsplash.com/photo-1619163413327-546fdb903195?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjJ8fGNoZXNzfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" className="max-w-sm rounded-lg shadow-2xl" />
     <div>
-      <h1 className="text-5xl font-bold">Welcome to Checkmate in five</h1>
-      <p className="py-6">Test your skill against AI or other players in this realistic chess game.</p>
+      <h1 className="text-5xl font-bold">Welcome to Checkmate in Five</h1>
+      <p className="py-6">Test your skill against AI or other players in this realistic chess game</p>
       <a href="/game-type" className="btn btn-primary">Begin</a>
     </div>
   </div>
